1b73dfba0370ae3ee61a620d0cea0d5a0e28848c
MohammedAlii80 <53688407+MohammedAlii80@users.noreply.github.com>
2023-02-05 01:37:36 +0200

    Add files via upload

--- crm_capex/models/models.py
+++ crm_capex/models/models.py
@@ -1,19 +1,38 @@
 from odoo import api, fields, models,_
 
 class capexcontact(models.Model):
-    _name = 'res.partner'
-    _inherit = 'res.partner'
+    _inherit = 'crm.lead'
+
+    contact_name = fields.Char(
+        'Applicant Full Name', tracking=30,
+        compute='_compute_contact_name', readonly=False, store=True)
 
     serial_number = fields.Text(string="serial number", required=False, )
     date = fields.Date(string="Date", required=False, )
+
     area_id = fields.Many2one(comodel_name="res.city", string="Area", required=False, )
     contact_name_person = fields.Text(string="Person", required=False, )
-    deal_amount = fields.Monetary(string='Deal Amount', tracking=True)
+    deal_amount = fields.Float(string='Deal Amount', tracking=True)
     deal_status = fields.Selection(string="Deal Status", selection=[('current', 'Current'), ('new', 'New'), ], required=False, )
-    Investments_in_millions = fields.Monetary(string='Investments In Millions', tracking=True)
+    Investments_in_millions = fields.Float(string='Investments In Millions', tracking=True)
     start_of_production = fields.Date(string="Start Of Production", required=False, )
     tax_tax = fields.Many2many('ir.attachment',string="Tax", required=True)
-    new_field = fields.Integer(string="People", required=False,compute="count_of_pepole" )
+    company_type = fields.Selection(string="Company Type", selection=[('One-person company (OPC)', 'One-person company (OPC)'), ('Limited laibility company(LLC)', 'Limited laibility company(LLC)'),
+                                                                      ('A company limited by shares','A company limited by shares'),
+                                                                      ('joint-stock company','joint-stock company'),
+                                                                      ('Partnership company','Partnership company'),
+                                                                      ('Limited Partnership company','Limited Partnership company'),
+                                                                      ('Sole proprietorsip','Sole proprietorsip'),
+                                                                      ], required=False, )
+    how_much = fields.Integer(string="How Much Do You Need To Borrow", required=False, )
+    year_business = fields.Selection(string="Year in business", selection=[('1-2years', '1-2years'),
+                                                                           ('3-5Years','3-5Years'),
+                                                                           ('5-10years','5-10years'),
+                                                                           ('10+years', '10+years'), ], required=False, )
+
+    industry_id = fields.Many2one(comodel_name="res.partner.industry", string="Company Industry")
+
+    # new_field = fields.Integer(string="People", required=False,compute="count_of_pepole" )
 
     # registry_id = fields.Many2many('ir.attachment', string="Commercial Registry", required=True)
 
@@ -23,18 +42,18 @@ class capexcontact(models.Model):
     #     print('>>>>>>>>',heuh)
     #     # print('>>>>>>>>',filt)
 
-    def count_of_pepole(self):
-        for rec in self:
-            count =self.env['sale.order'].search_count([('state','=','sale')])
-            rec.new_field = count
-
-            return {
-                'type': 'ir.actions.act_window',
-                'name': 'jooiwuqo',
-                'view_mode': 'tree,form',
-                'res_model': 'sale.order',
-                'domain': [('state', '=', 'sale')],
-                # 'context': "{'create': False}"
-            }
-
+    # def count_of_pepole(self):
+    #     for rec in self:
+    #         count =self.env['sale.order'].search_count([('state','=','sale')])
+    #         rec.new_field = count
+    #
+    #         return {
+    #             'type': 'ir.actions.act_window',
+    #             'name': 'jooiwuqo',
+    #             'view_mode': 'tree,form',
+    #             'res_model': 'sale.order',
+    #             'domain': [('state', '=', 'sale')],
+    #             # 'context': "{'create': False}"
+    #         }
+    #
 
